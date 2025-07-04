35efd4dcecdd57ba2dbd08d242e1e1fb4e922fad
Christian Aranguiz <chnicolas.aranguiz@gmail.com>
2025-07-04 18:55:53 -0400

    fix eslint rule disabled

--- frontend/src/app/emails-sent/page.tsx
+++ frontend/src/app/emails-sent/page.tsx
@@ -1,5 +1,6 @@
 "use client"
-
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+    
 import { Trash2, Star } from "lucide-react"
 import Sidebar from "@/components/Sidebar/Sidebar"
 import { useEffect, useState } from "react"
